377030b04d97bd84a2de6ead61417ad1e8df3f3a
Aron Homberg <info@aron-homberg.de>
2024-06-16 17:10:25 +0200

    chore: CORS

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,12 @@ dotEnvConfig();
 export const onRequest = defineMiddleware(async (context, next) => {
 	const { request } = context;
 
+	// pre-flight request
+	if (request.method === "OPTIONS") {
+		console.log("Middleware allowed pre-flight request, 200 OK");
+		return new Response(null, { status: 200 });
+	}
+
 	// CORS
 	const origin = request.headers.get("Origin");
 	if (origin) {
@@ -15,13 +21,6 @@ export const onRequest = defineMiddleware(async (context, next) => {
 			"GET, POST, PUT, DELETE, OPTIONS",
 		);
 		request.headers.set("Access-Control-Allow-Headers", "Content-Type");
-		request.headers.set("Access-Control-Allow-Credentials", "true");
-	}
-
-	// pre-flight request
-	if (request.method === "OPTIONS") {
-		console.log("Middleware allowed pre-flight request, 200 OK");
-		return new Response(null, { status: 200 });
 	}
 
 	let response: Response | undefined;
