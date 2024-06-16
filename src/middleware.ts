0c162943c6dc0e81b013f9485182737022260484
Aron Homberg <info@aron-homberg.de>
2024-06-16 16:45:16 +0200

    fix: CORS allow *

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,26 @@ import { defineMiddleware } from "astro:middleware";
 dotEnvConfig();
 
 export const onRequest = defineMiddleware(async (context, next) => {
+	const { request } = context;
+
+	// CORS
+	const origin = request.headers.get("Origin");
+	if (origin) {
+		request.headers.set("Access-Control-Allow-Origin", origin);
+		request.headers.set(
+			"Access-Control-Allow-Methods",
+			"GET, POST, PUT, DELETE, OPTIONS",
+		);
+		request.headers.set("Access-Control-Allow-Headers", "Content-Type");
+		request.headers.set("Access-Control-Allow-Credentials", "true");
+	}
+
+	// pre-flight request
+	if (request.method === "OPTIONS") {
+		console.log("Middleware allowed pre-flight request, 200 OK");
+		return new Response(null, { status: 200 });
+	}
+
 	let response: Response | undefined;
 	return await next();
 });
